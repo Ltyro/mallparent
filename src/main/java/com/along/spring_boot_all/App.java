4e20b996228d14143e24ce80c9e9d58563ad35ad
Lnstark <983536109@qq.com>
2020-05-30 15:05:35 +0800

    first commit

---
+++ src/main/java/com/along/spring_boot_all/App.java
@@ -0,0 +1,14 @@
+package com.along.spring_boot_all;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "ssh!" );
+    }
+}
